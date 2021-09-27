f002e28ac523aa9abe5e52354d1b80423d36c10c
gajackson09 <89426036+gajackson09@users.noreply.github.com>
2021-09-26 22:15:24 -0400

    Add files via upload

---
+++ package ex30/ex30.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Gabrielle Jackson
+ */
+
+ //this program creates a simple multiplication table
+ public class ex30{
+    public static void main(String[] args){
+        for(int i=1;i<=12;i++){
+            for(int j=1;j<=12;j++){
+                System.out.print((i*j)+"\t");
+            }//end of for
+        System.out.println();
+        }//end of for
+    }//end of main
+}//end of class
